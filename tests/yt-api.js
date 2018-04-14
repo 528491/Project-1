759516254f85ceef5de8b48c6fd07afc738285e5
unknown <mattcmej@gmail.com>
2018-04-14 02:30:06 -0700

    Decide to go with the 'get' method instead of ajax for retrieving data.

--- tests/yt-api.js
+++ tests/yt-api.js
@@ -13,47 +13,75 @@ Get a list of videos from a certain channel, then display them on a webpage.
 2. Take the uploads id and use that for the "playlistitems:list" method
     a. Will give us back all the videos on that channel.
 
+NOTE: I have decided to not do the API request the ajax way. Using the $get method is more modular.
+
 */
 
-// var queryURL = "https://www.googleapis.com/youtube/v3/channels";
+
+/////////////////////////////////////////////////////////////
+
+var channelName = "TechGuyWeb";
+
+$(document).ready(function() {
+
+
+    // first "get" request
+    $.get(
+        "https://www.googleapis.com/youtube/v3/channels", { 
+            part: 'contentDetails',
+            forUsername: channelName,
+            key: 'AIzaSyCqxm1KaFeRuiGu1vl6YcaDnmg7mU0mU_4'
+        }, function(data) {
+            // console.log(data);
+            $.each(data.items, function(i, item) {                              // index is "i"; items[i] === item
+                console.log(item);
+                pid = item.contentDetails.relatedPlaylists.uploads;
+
+                // the second "get" request
+                // getVids(pid);
+            })
+        }
+    );
+
+
+    // function getVids(pid) {
+
+    // }
+
+});
+
+
+/////////////////////////////////////////////////////////////
+
+// another way to phrase the above code using ajax:
+
+// var username = "thephilosophytube";
+// var part = "contentDetails";
+
+// // var queryURL = "https://www.googleapis.com/youtube/v3/channels?part=contentDetails&forUsername=" + username + "&key=AIzaSyCqxm1KaFeRuiGu1vl6YcaDnmg7mU0mU_4";
+
+// var queryURL = "https://www.googleapis.com/youtube/v3/channels?part=" + part + "&forUsername=" + username + "&key=AIzaSyCqxm1KaFeRuiGu1vl6YcaDnmg7mU0mU_4";
 
 // $.ajax({
-//     url: queryURL, 
+//     url: queryURL,
+//     method: "GET"
+// }).then(function(response) {
+//     console.log(response);
+//     pid = items[0].contentDetails.relatedPlaylists.uploads;
+//     getVids(pid);
 // })
 
-/////////////////////////////////////////////////////////////
+// // pid = playlist id
 
-// var channelName = "TechGuyWeb";
+// function getVids(pid) {
+//     part = "snippet";
 
-// $(document).ready(function() {
-//     $.get(
-//         "https://www.googleapis.com/youtube/v3/channels", { 
-//             part: 'contentDetails',
-//             forUsername: channelName,
-//             key: 'AIzaSyCqxm1KaFeRuiGu1vl6YcaDnmg7mU0mU_4'
-//         }, function(data) {
-//             $.each(data.items, function(i, item) {
-//                 console.log(item);
-
-//             })
-//         }
-//     );
-
-// });
-
-
-/////////////////////////////////////////////////////////////
-
-// another way to phrase the above code:
-
-var username = "thephilosophytube";
-
-var queryURL = "https://www.googleapis.com/youtube/v3/channels?part=contentDetails&forUsername=" + username + "&key=AIzaSyCqxm1KaFeRuiGu1vl6YcaDnmg7mU0mU_4";
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response) {
-    console.log(response);
-    // getVids(pid);
-})
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//     }).then(function(response) {
+//         console.log(response);
+//         pid = items[0].contentDetails.relatedPlaylists.uploads;
+//         getVids(pid);
+//     })
+// }
