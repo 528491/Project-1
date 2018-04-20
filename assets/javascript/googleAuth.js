eccb50a43dac8f21a03324b172a6bc26c27b305b
saraalinazari <sara.alinazari@gmail.com>
2018-04-20 13:18:31 -0700

    Update googleAuth.js

--- assets/javascript/googleAuth.js
+++ assets/javascript/googleAuth.js
@@ -1,13 +1,4 @@
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyC80yUq1_n29oubnc_nisGuDUUzYKhuLO8",
-    authDomain: "master-d1d42.firebaseapp.com",
-    databaseURL: "https://master-d1d42.firebaseio.com",
-    projectId: "master-d1d42",
-    storageBucket: "master-d1d42.appspot.com",
-    messagingSenderId: "1005294506678"
-  };
-  firebase.initializeApp(config);
+
 
 
 
