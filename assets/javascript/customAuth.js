ba6a8f18e298570bcb8e7c49b0b1fad0b76a1c33
saraalinazari <sara.alinazari@gmail.com>
2018-04-20 16:53:25 -0700

    Update customAuth.js

--- assets/javascript/customAuth.js
+++ assets/javascript/customAuth.js
@@ -10,61 +10,41 @@ var filter = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?
   }
   return (testresults);
 }
-$(document).ready(function(){
-firebase.auth().onAuthStateChanged(function(firebaseUser){
-console.log("Sara was here.");
-  
-  if(firebaseUser){
-      console.log(firebaseUser.uid);
-      console.log(firebaseUser.email);
-      // firebase.database().ref('/Users/'+firebaseUser.uid).once("value").then(function(response){
-      //     console.log("onAuthstate"+JSON.stringify(response));
-      //     $("#showSignup").hide();
-      //     $("#login").hide();
-      //     $("#SignUp").hide();
-      //     $("#showLogIn").hide();
-      //     // var temp1=firebase.database().child("Users").child(firebaseUser.uid).getValue("email");
-      //     //$("#userStatus").empty();
-      //     // console.log(temp1);
-          $("#welcome").html(firebaseUser.email +"is loggeded in");
-          $("#LogOut").show();
-      //     console.log("salam salam");
-     // });
-  }
-  else{
-      console.log('not logged in');
-      $("#welcome").html("");
-     // $("#showSignup").show();
-     $("#LogOut").hide();
-          $("#login").show();
-          $("#showLogIn").show();
-          $("#SignUp").show();
-      //    $("#userStatus").text("Not Logged in");
-      // firebase.auth().ref('/Users/'+user.userid).set({
-      //     username : username,
-      //     email : email,
-      //     password : password,
-      //     phoneno : phoneno,
-      //     address1 : address1,
-      //     address2 : address2,
-      //     zipcode : zipcode,
-      //     city : city,
-      //     displayname : displayname,
-      // });
-      // location.reload();
-  }
-});
+$(document).ready(function(e){
+
+    $("#LogOut").hide(); 
+    $("#submitSignUn").hide();
+    $("#login").hide();
+    firebase.auth().onAuthStateChanged(function(firebaseUser){
+      if(firebaseUser){
+        console.log(firebaseUser.uid);
+        console.log(firebaseUser.email);
+        $("#LogOut").show();
+        $("#submitSignUn").hide();
+        $("#login").hide();
+        $("#welcome").html(firebaseUser.email +"is loggeded in");  
+      }
+      else{
+        console.log('not logged in');
+        $("#LogOut").hide();
+        $("#login").show();
+        $("#submitSignUn").show();
+        // $("#welcome").html("You are not logged in");
+     
+      }
+    });
 });
 $("#login").on("click",function(){
-//$(this).disable = true;
-var email = $("#usernameLogin").val().trim(); 
-var pass1 = $("#passwordLogin").val().trim();
-// console.log(pass1);
-var pass = pass1.toString(); 
-firebase.auth().signInWithEmailAndPassword(email,pass).then(function(user){
+
+  var email = $("#usernameLogin").val().trim(); 
+  var pass1 = $("#passwordLogin").val().trim();
+
+  var pass = pass1.toString(); 
+
+  firebase.auth().signInWithEmailAndPassword(email,pass).then(function(user){
     console.log(JSON.stringify(user));
     $("#LogOut").show();
-}).catch(function(error){
+  }).catch(function(error){
     if(error.code == "auth/invalid-email"){
       alert("wrong pass or username");
     }
@@ -76,51 +56,28 @@ firebase.auth().signInWithEmailAndPassword(email,pass).then(function(user){
     }
     console.log(error.code);
     console.log(error.message);
+  });
 });
-});
-$("#SignUp").on("click",function(){
-//  $(this).disable = true;
-$("#showSignup").show();
-$("#showLogIn").hide();
-$("#SignUp").hide();
-$("#LogOut").hide();
-$("#login").hide();
-});
+
 $("#submitSignUn").on("click",function(){
-//$(this).disable = true;
-var email =$("#usernameLogin").val();
-console.log(email+":"+ checkEmail(email)); 
-var password = $("#passwordLogin").val();
-// var phoneno = $("#phoneno").val().trim();
-// var address1 = $("#address1").val().trim();
-// var address2 = $("#address2").val().trim();
-// var zipcode = $("#zipcode").val().trim();
-// var city= $("#city").val().trim();
-// var displayname= $("#displayname").val().trim();
-// var state = $("#inputState").val().trim();
-signUp(email,password);
+
+    var email =$("#usernameLogin").val();
+    console.log(email+":"+ checkEmail(email)); 
+    var password = $("#passwordLogin").val();
+    signUp(email,password);
 });
 $("#LogOut").on("click",function(){
-firebase.auth().signOut();
-$(this).disable = true;
-//$("#showSignup").hide();
-          $("#login").hide();
-          $("#SignUp").hide();
+    firebase.auth().signOut();
+    $("#login").show();
+    $("#SignUp").show();
 });
 function signUp(email,password){
-firebase.auth().createUserWithEmailAndPassword(email,password).then(function(user){
-useruid = user.uid;
-firebase.database().ref('/Users/'+useruid).push({
-  //username : username,
-  email : email,
-  password : password
-  // phoneno : phoneno,
-  // address1 : address1,
-  // address2 : address2,
-  // zipcode : zipcode,
-  // city : city,
-  // state : state,
-  // displayname : displayname
+    firebase.auth().createUserWithEmailAndPassword(email,password).then(function(user){
+    seruid = user.uid;
+    firebase.database().ref('/Users/'+useruid).push({
+    email : email,
+    password : password
+  
 }).catch(function(error){
   if(error.code.toString() == "auth/weak-password"){
       alert("Your password is so weak. Please, select another password.");
@@ -131,24 +88,13 @@ firebase.database().ref('/Users/'+useruid).push({
   console.log("SALAM:" ,error.message);
   console.log(error.code);
 }).then(function(){
-  location.reload();//window.location.href = "index.html";
+  location.reload();
 });
 });
-//location.reload();
+
+$("#SignUp").hide();
+$("#LogOut").show();
+$("#login").hide();
 }
-// firebase.auth().signInWithEmailAndPassword(email, password).then(function(user){
-//     console.log(firebase.database().ref('/Users/'+user.userid))
-// }).catch(function(error){
-//     console.log(error.code);
-//     console.log(error.message);
-// });
-// $("#login").on("click",function(){
-//     var username = $("username").val();
-//     var password= $("password").val();
-//     firebase.auth().signInWithEmailAndPassword(email,password).then(function(user){
-//         console.log(user);
-//     }).catch(function(){
-//         console.log(error.code);
-//         console.log(error.message);
-//     });
-// });
+
+
