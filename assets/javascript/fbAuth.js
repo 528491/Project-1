547c41505c6690038f524ad5848a8b37eac3a440
Ngoc Nguyen <rnguyen05@gmail.com>
2018-04-20 17:48:33 -0700

    Update fbAuth.js

--- assets/javascript/fbAuth.js
+++ assets/javascript/fbAuth.js
@@ -24,7 +24,26 @@ var userName = "";
 var userEmail = "";
 var userImage = "";
 
-//var fProvider = new firebase.auth.FacebookAuthProvider();
+var fProvider = new firebase.auth.FacebookAuthProvider();
+
+firebase.auth().signInWithPopup(fprovider).then(function(result) {
+  // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+  var token = result.credential.accessToken;
+  // The signed-in user info.
+  var user = result.user;
+  // ...
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
+
+
 
 window.fbAsyncInit = function() {
   FB.init({
