2e838299497d56113b857ef636db7afe35468fe6
Ngoc Nguyen <rnguyen05@gmail.com>
2018-04-20 19:54:49 -0700

    Update fbAuth.js

--- assets/javascript/fbAuth.js
+++ assets/javascript/fbAuth.js
@@ -26,7 +26,7 @@ var userImage = "";
 
 var fProvider = new firebase.auth.FacebookAuthProvider();
 
-firebase.auth().signInWithPopup(fprovider).then(function(result) {
+firebase.auth().signInWithPopup(fProvider).then(function(result) {
   // This gives you a Facebook Access Token. You can use it to access the Facebook API.
   var token = result.credential.accessToken;
   // The signed-in user info.
